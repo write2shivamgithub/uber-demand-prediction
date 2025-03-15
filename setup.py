4c95a5bc0edccfbefbc30e1f539d2e3d4d6c7643
write2shivamgithub <shivamshankaraacharya@gmail.com>
2025-03-15 18:14:33 +0530

    performed EDA on dataset using Dask

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A project on predicting number of pickups within regions for uber drivers',
+    author='Shivam',
+    license='',
+)
